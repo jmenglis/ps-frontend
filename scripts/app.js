0e9fc6199ad437602dd214df2a1f93f282ca226d
Josh English <josh@joshenglish.com>
2016-05-22 00:16:51 -0500

    Completed NPM Init

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+window.onload =  function() {
+  console.log("App.js has loaded");
+  // $.ajax({
+  //   type: 'get',
+  //   datatype: 'json',
+  //   url: 'http://applicant.pointsource.us/api/testUser/573f5d0b7e3d61136595a180',
+  //   success: function(response) {
+  //     console.log(response)
+  //   },
+  //   error: function(e) {
+  //     console.log(e);
+  //   }
+  // })
+
+  // $.getJSON('http://applicant.pointsource.us/api/testUser/573f5d0b7e3d61136595a180', function(response) {
+  //   console.log(response);
+  // });
+}
+// angular.module('myApp', [])
+//   .controller("UserController", function($scope, $http) {
+//     $scope.init = function () {
+//       console.log("Fetch:")
+//       fetch();
+//     }
+//   function fetch() {
+//     $http.get("http://applicant.pointsource.us/api/testUser/573f5d0b7e3d61136595a180")
+//     .then(function(response) {
+//       console.log(response);
+//     })
+//   }
+//   });
